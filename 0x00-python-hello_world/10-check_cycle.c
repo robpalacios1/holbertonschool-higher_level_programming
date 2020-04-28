1f2dfe4e52ede15ad97fb3e5297dd1b20b7ee75e
Roberto <robpalacios1@hotmail.com>
2020-04-27 20:24:52 -0500

    fix code inside emacs

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,11 @@
  */
 int check_cycle(listint_t *list)
 {
-
 	listint_t *slow = list;
 	listint_t *fast = list->next;
 
 	if (list == NULL)
-	{
 		return (0);
-	}
 
 	for (; fast != NULL && fast->next != NULL;
 	     slow = slow->next, fast = fast->next->next)
