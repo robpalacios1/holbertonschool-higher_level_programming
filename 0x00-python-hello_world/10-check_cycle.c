a2a5a96b730646802e16f0bf86b1f7d26cd9a99a
Roberto <robpalacios1@hotmail.com>
2020-04-27 21:42:59 -0500

    fix code inside emacs

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,26 @@
 #include "lists.h"
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
+ * check_cycle - Function in C that checks if
+ * a singly linked list has a cycle in it
+ * @list: Pointer to the listint_t struct
  *
- * @list: pointer to head node
- *
- * Return: 0 if no cycle, 1 if cycle
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *fast, *slow;
 
-	listint_t *slow = list;
-	listint_t *fast = list->next;
-
-	if (list == NULL)
-		return (0);
-
-	for (; fast != NULL && fast->next != NULL;
-	     slow = slow->next, fast = fast->next->next)
+	if (list == NULL || list->next == NULL)
 	{
+		return (0);
+	}
+
+	slow = list;
+	fast = list;
+	while (fast != NULL && slow != NULL && fast->next != NULL)
+	{
+		slow  = slow->next;
+		fast = fast->next->next;
 		if (slow == fast)
 		{
 			return (1);
