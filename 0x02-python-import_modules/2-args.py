b05643248a4b0cd835671d81b53e62c76da7ea57
Roberto <robpalacios1@hotmail.com>
2020-04-30 14:27:47 -0500

    fix code inside emacs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
