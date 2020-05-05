5260d8a23366ddc90bac89ed9ac0bdd59a549d10
Roberto <robpalacios1@hotmail.com>
2020-05-05 14:39:35 -0500

    add 6-main.py and 6-print_matrix_integer.py files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
