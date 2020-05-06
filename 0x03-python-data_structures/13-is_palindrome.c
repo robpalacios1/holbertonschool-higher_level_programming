9d059cd0c12fff9f8443be8c00b0c678e41689a0
Roberto <robpalacios1@hotmail.com>
2020-05-05 19:14:09 -0500

    add 13-is_palindrome.c 13-main.c linked_lists.c and lists.h files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int is_palindrome(listint_t **head)
+{
+	int size;
+	int options[2048];
+	int i;
+
+	size = 0;
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	while(*head != NULL)
+	{
+		size++;
+		options[size - 1] = (*head)->n;
+		head = &(*head)->next;
+	}
+
+	for (i = 0; i < size / 2; i++)
+	{
+		if (options[i] != options[size - i - 1])
+			return (0);
+	}
+
+	return (1);
+}
