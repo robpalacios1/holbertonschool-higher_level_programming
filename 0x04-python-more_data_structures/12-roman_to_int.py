247b1aec577602a275c606de87f985ad35253e91
Roberto <robpalacios1@hotmail.com>
2020-05-07 18:47:27 -0500

    add 12-main.py and 12-roman_to_int.py files

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    a = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+             'M': 1000}
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    for i in roman_string:
+        if i in roman:
+            a = a + roman[i]
+    for compare in range (0, len(roman_string) - 1):
+        if roman[roman_string[compare]] < roman[roman_string[compare + 1]]:
+            a = a - 2
+    return a
