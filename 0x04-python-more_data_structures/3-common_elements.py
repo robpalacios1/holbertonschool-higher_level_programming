e1d76fab03dcec5828a733314386a45b55502e92
Roberto <robpalacios1@hotmail.com>
2020-05-07 13:43:48 -0500

    add retval function

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a = []
+    retval = []
     for i in set_1:
         for j in set_2:
             if i == j:
-                a.append(j)
-        return a
+                retval.append(j)
+        return retval
