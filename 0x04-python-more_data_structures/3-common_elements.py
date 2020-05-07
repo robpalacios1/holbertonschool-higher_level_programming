c62e4eee33b47a8e9904989e3f295b982539b9ed
Roberto <robpalacios1@hotmail.com>
2020-05-07 13:50:49 -0500

    fix code inside emacs

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    retval = []
+    a = []
     for i in set_1:
         for j in set_2:
             if i == j:
-                retval.append(j)
-    return retval
+                a.append(j)
+    return a
