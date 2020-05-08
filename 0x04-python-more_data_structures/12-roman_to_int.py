61338e1d8772d09d1d95d216a6575b247297f628
Roberto <robpalacios1@hotmail.com>
2020-05-07 19:03:33 -0500

    fix code inside emacs

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     a = 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-             'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) != str:
         return 0
     for i in roman_string:
