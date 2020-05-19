6dbde07047d319c725aad9c867b77a181c562297
Roberto <robpalacios1@hotmail.com>
2020-05-19 16:13:07 -0500

    add Docstring to code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''Square class'''
+
+
 class Square:
+    '''Private instance attribute size'''
     def __init__(self, size):
+        '''inalitation with size'''
         self.__size = size
