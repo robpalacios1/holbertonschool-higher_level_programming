2e59da7cb94b258641f548fac7f8f5a490bc9050
Roberto <robpalacios1@hotmail.com>
2020-05-19 16:35:45 -0500

    add 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+class Square:
+    '''private instance attribute size'''
+    def __init__(self, size=0):
+        '''inalitation with size'''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''return square area'''
+        return self.__size ** 2
