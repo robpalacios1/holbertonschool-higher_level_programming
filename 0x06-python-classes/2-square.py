1963292bb5626fac27b42bf706667d291d64a745
Roberto <robpalacios1@hotmail.com>
2020-05-19 16:17:40 -0500

    add Docstring into a code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'''Square class'''
+
+
 class Square:
+    '''private instance atributte size'''
     def __init__(self, size=0):
+        '''inalitation with size'''
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
