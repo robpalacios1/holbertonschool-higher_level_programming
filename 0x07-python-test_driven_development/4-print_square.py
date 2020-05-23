a8d132ed5918947672254c2ce69e2f4b7377a568
Roberto <robpalacios1@hotmail.com>
2020-05-23 16:24:36 -0500

    fix code inside emacs

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 '''class print_square'''
 
+
 def print_square(size):
     ''' Print a square with size '''
     if type(size) is not int:
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
-
     for i in range(size):
         print('#' * size)
