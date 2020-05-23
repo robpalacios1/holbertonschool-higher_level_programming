432cad4eed27667f742a4755632adb10d47eec82
Roberto <robpalacios1@hotmail.com>
2020-05-23 18:09:34 -0500

    fix code inside emacs

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,7 @@
 def say_my_name(first_name, last_name=""):
     '''function that prints My name is <first_name> <last_name>'''
     if type(first_name) is not str:
-        raise TypeError("fisrt_name must be a string")
+        raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
