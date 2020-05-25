285927dbfb0cf45a70765e0b71b7e109a2279def
Roberto <robpalacios1@hotmail.com>
2020-05-25 18:37:33 -0500

    fix code by pep8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     '''rectangle class'''
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         '''initialize rectangle value
            width and height
