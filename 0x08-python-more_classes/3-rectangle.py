eb27090fb0bc4482028861704d3b0e8c4d19995c
Roberto <robpalacios1@hotmail.com>
2020-05-25 18:32:37 -0500

    fix code inside emacs

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width * 2) + (self.__height * 2)
