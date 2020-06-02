67d6793d2d1b705f2e85b236184c14a2ada4c050
Roberto <robpalacios1@hotmail.com>
2020-06-02 18:15:58 -0500

    add stringdoc

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,12 @@
 #!/bin/usr/python3
+'''Same class or inherit from module'''
+
+
 def is_kind_of_class(obj, a_class):
+    '''return if the object is an instance of, or if the object
+       is an instance of a class that inherited from, the specified class;
+       otherwise False.
+    '''
     if isinstance(obj, a_class):
         return True
     else:
