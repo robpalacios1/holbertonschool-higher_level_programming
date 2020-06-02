b76420f401de217e68a3b89184eb296ef8b4bb21
Roberto <robpalacios1@hotmail.com>
2020-06-02 18:16:25 -0500

    add stringdoc

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''inherits_from module'''
+
+
 def inherits_from(obj, a_class):
+    '''return if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+    '''
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
