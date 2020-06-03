611475a5eb811cb19a97603a905b4655c87cdd05
Roberto <robpalacios1@hotmail.com>
2020-06-03 17:54:06 -0500

    add docstring

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''Number of lines module'''
+
+
 def number_of_lines(filename=""):
+    '''return number of lines of a text'''
     numlines = 0
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
