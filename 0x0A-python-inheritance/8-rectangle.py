0744fe410158ff078ff529552c942943628f715a
Roberto <robpalacios1@hotmail.com>
2020-06-02 20:19:57 -0500

    add docstring

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
+'''Rectangle module'''
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'''import from exercise 7-base_geometry'''
+
 
 class Rectangle(BaseGeometry):
+    '''class Rectangle'''
     def __init__(self, width, height):
+        '''initialize constructor'''
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
