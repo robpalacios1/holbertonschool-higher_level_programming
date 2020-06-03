3fa5922d59b95bdaddec24a94c16479dd708bc4a
Roberto <robpalacios1@hotmail.com>
2020-06-02 22:21:02 -0500

    add docstring

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+'''MyInt module'''
 class MyInt(int):
+    '''class MyInt'''
     def __new__(cls, *args, **kwargs):
+        '''create a new instance'''
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
+        '''it was != is now =='''
         return int(self) != other
 
     def __ne__(self, other):
+        '''it was == is now !='''
         return int(self) == other
