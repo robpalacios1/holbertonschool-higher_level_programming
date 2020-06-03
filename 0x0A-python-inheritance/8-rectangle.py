dc28ca5946dcff9f73a92d2ec5ad1f2398ac30c2
Roberto <robpalacios1@hotmail.com>
2020-06-02 20:22:42 -0500

    fix docstring lines

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,13 +3,12 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-'''import from exercise 7-base_geometry'''
 
 
 class Rectangle(BaseGeometry):
     '''class Rectangle'''
     def __init__(self, width, height):
-        '''initialize constructor'''
+        '''initialize constructor width and height'''
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
