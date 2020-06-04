48b99fd39beda0ff46d1e10ca4200f4fa5b4a3d7
Roberto <robpalacios1@hotmail.com>
2020-06-03 21:32:48 -0500

    add 13-student.py file

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Student to disk and reload module'''
+
+
+class Student:
+    '''student class'''
+    def __init__(self, first_name, last_name, age):
+        '''initialize atributes first_name last_name age'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation of instance'''
+        new = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    new[i] = self.__dict__
+                else:
+                    continue
+            return new
+
+    def reload_from_json(self, json):
+        '''replaces all attributes of student instance'''
+        self.__dict__ = json
