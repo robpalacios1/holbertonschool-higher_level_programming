4b722f0904dec330bc7f71e72ae15a2a0a8dff77
Roberto <robpalacios1@hotmail.com>
2020-06-06 20:47:38 -0500

    add task9 Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,3 +116,13 @@ class Rectangle(Base):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        '''return a dictionary'''
+        new_dict = {}
+        new_dict['id'] = self.id
+        new_dict['width'] = self.width
+        new_dict['height'] = self.height
+        new_dict['x'] = self.x
+        new_dict['y'] = self.y
+        return new_dict
