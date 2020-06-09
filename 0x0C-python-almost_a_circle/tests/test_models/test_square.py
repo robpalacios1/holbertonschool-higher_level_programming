3b0c782411c77fbc9c7f77a3864bbbe2ae51d646
Roberto <robpalacios1@hotmail.com>
2020-06-09 16:22:09 -0500

    add more unittest

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,156 +1,74 @@
 #!/usr/bin/python3
-'''unittest square module'''
-
-
+'''unit tests for square module'''
 import unittest
-from io import StringIO
-import sys
-import contextlib
-import pep8
-from models.rectangle import Rectangle
 from models.square import Square
-from models.base import Base
 
 
 class TestSquare(unittest.TestCase):
-    """ unittesting class Rectangle task:
-        10. And now, the Square!, 11. Square size """
+    '''test cases for square'''
 
-    def test_pep8_model(self):
-        """Tests for pep8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/square.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_createsquare(self):
+        '''create new square test'''
+        new = Square(2, 2, id=60)
+        self.assertEqual(str(new), '[Square] (60) 2/0 - 2')
 
-    def test_pep8_test(self):
-        """Tests for pep8"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_square.py'])
-        self.assertEqual(result.total_errors, 1,
-                         "Found code style errors (and warnings).")
+    def test_size(self):
+        new = Square(6)
+        self.assertEqual(new.size, 6)
+        self.assertEqual(new.width, 6)
 
-    def test_00_documentation(self):
-        """Test to see if documentation is created and correct"""
-        self.assertTrue(hasattr(Square, "__init__"))
-        self.assertTrue(Square.__init__.__doc__)
-        self.assertTrue(hasattr(Square, "size"))
-        self.assertTrue(Square.size.__doc__)
-        self.assertTrue(hasattr(Square, "x"))
-        self.assertTrue(Square.x.__doc__)
-        self.assertTrue(hasattr(Square, "y"))
-        self.assertTrue(Square.y.__doc__)
-        self.assertTrue(hasattr(Square, "__str__"))
-        self.assertTrue(Square.__str__.__doc__)
-        self.assertTrue(hasattr(Square, "update"))
-        self.assertTrue(Square.update.__doc__)
-        self.assertTrue(hasattr(Square, "to_dictionary"))
-        self.assertTrue(Square.to_dictionary.__doc__)
+    def test_no_size(self):
+        with self.assertRaises(TypeError):
+            new = Square()
 
-    def test_aa_instance(self):
-        """ check inherence and subclass """
-        x = Square(5)
-        self.assertTrue(
-            type(x) is Square and issubclass(type(x), Rectangle))
+    def test_invalid_size(self):
+        with self.assertRaises(ValueError):
+            new = Square(-8)
 
-    def test_aa_private_size(self):
-        """ validate private attribute for __size """
-        with self.assertRaises(AttributeError):
-            print(Square(5).__width)
+    def string_as_input(self):
+        with self.assertRaises(TypeError):
+            new = Square("Danny")
 
-    def test_aa_getter(self):
-        """ test getters and setters method """
-        gt = Square(2, 5)
-        self.assertEqual(gt.size, 2)
+    def list_as_input(self):
+        with self.assertRaises(TypeError):
+            new = Square([1, 2, 3])
 
-    def test_aa_setters(self):
-        """ test setters and setters method """
-        gt = Square(4, 5)
-        gt.width = 30
-        self.assertEqual(gt.width, 30)
+    def tuple_as_input(self):
+        with self.assertRaises(TypeError):
+            new = Square((6, 7))
 
-    def test_aa_str(self):
-        """ test 1 """
-        r1 = Square(2, 2, 2, 2)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            print(r1)
-        self.assertEqual(f.getvalue(), "[Square] (2) 2/2 - 2\n")
+    def dict_as_input(self):
+        with self.assertRaises(TypeError):
+            new = Square({'size': 30})
 
-    def test_aa_display1(self):
-        """This function tests the display function"""
-        r1 = Square(3)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            r1.display()
-        self.assertEqual(f.getvalue(), "###\n###\n###\n")
+    def test_x_values(self):
+        square1 = Square(2, 3, 4, 78)
+        square2 = Square(5, 6, 1, 45)
+        square3 = Square(9, 2, 7, 98)
+        square4 = Square(5, 8, 9, 44)
+        square5 = Square(1, 9, 5, 77)
+        self.assertEqual(square1.x, 3)
+        self.assertEqual(square2.x, 6)
+        self.assertEqual(square3.x, 2)
+        self.assertEqual(square4.x, 8)
+        self.assertEqual(square5.x, 9)
 
-    def test_aa_display2(self):
-        """This function tests the display function"""
-        r1 = Square(3, 1, 3)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            r1.display()
-        self.assertEqual(f.getvalue(), "\n\n\n ###\n ###\n ###\n")
+    def test_y_values(self):
+        square1 = Square(2, 3, 4, 78)
+        square2 = Square(5, 6, 1, 45)
+        square3 = Square(9, 2, 7, 98)
+        square4 = Square(5, 8, 9, 44)
+        square5 = Square(1, 9, 5, 77)
+        self.assertEqual(square1.y, 4)
+        self.assertEqual(square2.y, 1)
+        self.assertEqual(square3.y, 7)
+        self.assertEqual(square4.y, 9)
+        self.assertEqual(square5.y, 5)
 
-    def test_aa_error(self):
-        """ handle error """
-        strs1 = "takes from 2 to 5 positional arguments but 6 were"
-        strs2 = "missing 1 required positional argument: 'size'"
-        with self.assertRaisesRegex(TypeError, strs1):
-            Square(22, 3, 4, 3, 5)
-        with self.assertRaisesRegex(TypeError, strs2):
-            Square()
-
-    def test_ab_args(self):
-        """ testing positional arguments """
-        r1 = Square(50)
-        self.assertEqual(r1.size, 50)
-        r1.update(10, 22, 14, 5)
-        self.assertEqual(r1.size, 22)
-        self.assertEqual(r1.id, 10)
-        self.assertEqual(r1.x, 14)
-        self.assertEqual(r1.y, 5)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            print(r1)
-        self.assertEqual(f.getvalue(), "[Square] (10) 14/5 - 22\n")
-
-    def test_ab_kwargs(self):
-        """ testing positional arguments """
-        r1 = Square(50)
-        self.assertEqual(r1.size, 50)
-        r1.update(y=5, id=10, x=14, size=22)
-        self.assertEqual(r1.size, 22)
-        self.assertEqual(r1.id, 10)
-        self.assertEqual(r1.x, 14)
-        self.assertEqual(r1.y, 5)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            print(r1)
-        self.assertEqual(f.getvalue(), "[Square] (10) 14/5 - 22\n")
-
-    def test_ac_load_dict(self):
-        """ testing positional arguments """
-        new_dict = {'x': 14, 'y': 5, 'id': 10, 'size': 22}
-        r1 = Square(50)
-        self.assertEqual(r1.size, 50)
-        r1.update(**new_dict)
-        self.assertEqual(r1.size, 22)
-        self.assertEqual(r1.id, 10)
-        self.assertEqual(r1.x, 14)
-        self.assertEqual(r1.y, 5)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            print(r1)
-        self.assertEqual(f.getvalue(), "[Square] (10) 14/5 - 22\n")
-
-    def test_ac_type_dict(self):
-        """ testing positional arguments """
-        new_dict = {'x': 14, 'y': 5, 'id': 10, 'size': 22}
-        r1 = Square(50)
-        r1.update(**new_dict)
-        f = StringIO()
-        with contextlib.redirect_stdout(f):
-            print(type(r1.to_dictionary()))
-        self.assertEqual(f.getvalue(), "<class 'dict'>\n")
+    def test_update(self):
+        new = Square(1, 2, 3, 4)
+        new.update(5, 6, 7, 8)
+        self.assertEqual(new.id, 5)
+        self.assertEqual(new.size, 6)
+        self.assertEqual(new.x, 7)
+        self.assertEqual(new.y, 8)
