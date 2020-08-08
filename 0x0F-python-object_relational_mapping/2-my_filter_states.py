6db4c542193de6e90ba8c8189af7cf325037c1aa
Roberto <robpalacios1@hotmail.com>
2020-08-08 15:13:04 -0500

    fix comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
-'''script that lists all states from database 'hbtn_0e_0_usa'''''
+"""
+script that lists all states from database 'hbtn_0e_0_usa'
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-        db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3])
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id asc"
                     .format(sys.argv[4]))
