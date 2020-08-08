a99f878b354fbb5d7f1e86ca6a91858976fd2e88
Roberto <robpalacios1@hotmail.com>
2020-08-08 18:40:20 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY '{}' ORDER BY id".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:
