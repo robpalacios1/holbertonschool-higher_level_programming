5fe165de2ba3490e46d7ce8570eb6267955c7ce9
Roberto <robpalacios1@hotmail.com>
2020-08-08 18:50:58 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument
+script that lists all states from database 'hbtn_0e_0_usa'
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
-    user = argv[1]
-    password = argv[2]
-    database = argv[3]
-    name = argv[4]
-    query = "SELECT * FROM states ORDER BY id ASC".format(name)
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=user,
-                         passwd=password, db=database)
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=argv[1], passwd=argv[2], db=argv[3])
+    
     cur = db.cursor()
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY '{}' ORDER BY id".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == name:
+        if row[1] == argv[4]:
             print(row)
     cur.close()
     db.close()
