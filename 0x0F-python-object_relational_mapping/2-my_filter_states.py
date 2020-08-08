9ec4ca0d8c526dac1fb125705b939383de8c5847
Roberto <robpalacios1@hotmail.com>
2020-08-08 00:46:11 -0500

    fix identation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,12 @@ import sys
 if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
-            cur = db.cursor()
-            cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id asc"
-                        .format(sys.argv[4]))
-            rows = cur.fetchall()
-            for row in rows:
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id asc"
+                    .format(sys.argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
                 if row[1] == sys.argv[4]:
                     print(row)
-            cur.close()
-            db.close()
+        cur.close()
+        db.close()
