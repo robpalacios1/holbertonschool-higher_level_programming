70c04f3f029b656203d666ee81ac5e70ccd20a75
Roberto <robpalacios1@hotmail.com>
2020-08-08 16:02:42 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY \ id asc"
+        cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id asc"
                     .format(sys.argv[4]))
         rows = cur.fetchall()
         for row in rows:
