4abccd3b3eaa571f46f5217d3762cd93e2865635
Roberto <robpalacios1@hotmail.com>
2020-08-08 15:45:02 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,14 +3,14 @@
 script that lists all states from database 'hbtn_0e_0_usa'
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY id asc"
-                    .format(sys.argv[4]))
+        cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY \ id asc"
+                    .format(argv[4]))
         rows = cur.fetchall()
         for row in rows:
                 if row[1] == sys.argv[4]:
