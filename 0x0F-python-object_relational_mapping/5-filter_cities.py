98f271c0751c803a7dce90507bd97463a207dbdc
Roberto <robpalacios1@hotmail.com>
2020-08-08 16:14:33 -0500

    fix code indentation

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-'''script that lists all states from database 'hbtn_0e_0_usa'''''
-
+"""
+script that lists all states from database 'hbtn_0e_0_usa'
+"""
 import MySQLdb
 import sys
 
@@ -9,10 +9,10 @@ if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-                cur.execute("SELECT cities.name FROM cities\
-                JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s ORDER BY cities.id",
-                                (sys.argv[4],))
+        cur.execute("SELECT cities.name FROM cities\
+        JOIN states ON cities.state_id = states.id\
+        WHERE states.name = %s ORDER BY cities.id",
+                    (sys.argv[4],))
         rows = cur.fetchall()
         retval = ""
         for row in range(len(rows)):
