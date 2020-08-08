052fb60ba09a80240ce368b194f715d86ce26880
Roberto <robpalacios1@hotmail.com>
2020-08-08 17:08:00 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,14 +12,14 @@ from sys import argv
 
 
 if __name__ == "__main__":
-        eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-        Base.metadata.create_all(eng)
-        Session = sessionmaker(bind=eng)
-        session = Session()
-        cali = State(name="California")
-        cali.cities = [City(name="San Francisco")]
-        session.add(cali)
-        session.commit()
-        session.close()
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    cali = State(name="California")
+    cali.cities = [City(name="San Francisco")]
+    session.add(cali)
+    session.commit()
+    session.close()
