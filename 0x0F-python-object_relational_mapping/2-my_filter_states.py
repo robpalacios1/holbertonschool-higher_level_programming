c9312098702bc35dac3ae3b9753af5eb08567970
Roberto <robpalacios1@hotmail.com>
2020-08-08 00:56:19 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,7 @@
 '''script that lists all states from database 'hbtn_0e_0_usa'''''
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
         db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1],
