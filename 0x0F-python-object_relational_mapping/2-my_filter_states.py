ad24942b118da21d0fee58986ac5f8dcdd89c04f
Roberto <robpalacios1@hotmail.com>
2020-08-08 18:28:48 -0500

    fix code inside emacs

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,13 +6,9 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = {
-        'host': "localhost", 
-        'port': 3306,
-        'user': sys.argv[1],
-        'passwd': sys.argv[2],
-        'db': sys.argv[3]
-    }
+    db = (host="localhost", port=3306,
+                user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
     db = MySQLdb.connect()
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name\
