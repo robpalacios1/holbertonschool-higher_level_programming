cb0f0be26714d60e435b58bd1a4a62b2a9e19156
Roberto <robpalacios1@hotmail.com>
2020-08-08 19:44:03 -0500

    fix code indentation

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -12,13 +12,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-        eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-        Base.metadata.create_all(eng)
-        Session = sessionmaker(bind=eng)
-        session = Session()
-        rows = session.query(City).all()
-        for city in rows:
-            print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-        session.close()
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    rows = session.query(City).all()
+    for city in rows:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
