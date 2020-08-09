45bab6e21e0e6019631559eb98b95a79e1a268a0
Roberto <robpalacios1@hotmail.com>
2020-08-08 19:45:24 -0500

    fix code insid emacs

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,15 +12,15 @@ from sys import argv
 
 
 if __name__ == "__main__":
-        eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-        Base.metadata.create_all(eng)
-        Session = sessionmaker(bind=eng)
-        session = Session()
-        rows = session.query(State).all()
-        for state in rows:
-            print("{}: {}".format(state.id, state.name))
-            for city in state.cities:
-                print("    {}: {}".format(city.id, city.name))
-        session.close()
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    rows = session.query(State).all()
+    for state in rows:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+    session.close()
