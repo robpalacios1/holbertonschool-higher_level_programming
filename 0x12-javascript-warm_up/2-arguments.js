b937557f5e446ac4e08c65bdad0648e84b2f2265
Roberto <robpalacios1@hotmail.com>
2020-09-15 15:35:03 -0500

    fix code inside emacs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,6 +5,6 @@ if (process.argv.length <= 2) {
     if (process.argv.length > 3) {
 	console.log('Arguments found');
     } else {
-	console.log('Argument found')
+	console.log('Argument found');
     }
 }
