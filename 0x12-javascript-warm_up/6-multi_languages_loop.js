31ce7fad525f70c1b9abe303de324ad1e2f663a8
root <robpalacios1@hotmail.com>
2020-09-16 00:01:22 +0000

    add code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let length = arr.length;
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const length = arr.length;
 let i = 0;
 
-for(i = 0; i < length; i++){
-    console.log(arr[i]);
+for (i = 0; i < length; i++) {
+  console.log(arr[i]);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const size = Math.floor(process.argv[2]);
-if(isNaN(size)) {
-    console.log('Missing size');
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-    for(let i = 0; i < size; i++) {
-	let row = '';
-	for(let j = 0; j < size; j++)
-	row = row + 'X'
-	console.log(row)
-    }
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) { row = row + 'X'; }
+    console.log(row);
+  }
 }
