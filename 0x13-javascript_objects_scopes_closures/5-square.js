838da3020d0fc9337eac921f2a460588fadbad63
Roberto <robpalacios1@hotmail.com>
2020-09-21 13:11:56 -0500

    add 5-square.js file

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Square require('./4-rectangle') {
+module.exports = class Square extends require('./4-rectangle') {
     constructor (size) {
 	super(size, size)
     }
